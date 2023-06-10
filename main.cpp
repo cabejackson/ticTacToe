ae565219fa030db83b0d6429d531943804d3510e
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2023-06-10 14:14:32 -0600

    Add clear screen instead of filling the terminal

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 char matrix[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 char player = 'X';
 void DrawGameboard(){
+    system("clear screen");
     cout << "Tic Tac Toe v1.0" << endl;
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
